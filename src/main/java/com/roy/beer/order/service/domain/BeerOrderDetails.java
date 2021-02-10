2e2f5d3b810b68a541209676d4d41c1896dd8006
Anirban Roy <aroyiem@gmail.com>
2021-02-10 18:17:10 +0000

    added upc to beerorder detail

--- src/main/java/com/roy/beer/order/service/domain/BeerOrderDetails.java
+++ src/main/java/com/roy/beer/order/service/domain/BeerOrderDetails.java
@@ -20,20 +20,24 @@ public class BeerOrderDetails extends BaseEntity {
     private BeerOrder beerOrder;
 
     private UUID beerId;
+    private String upc;
     private Integer orderQuantity = 0;
     private Integer quantityAllocated = 0;
 
     @Builder
-    public BeerOrderDetails(UUID id, Long version,
+    public BeerOrderDetails(UUID id,
+                            Long version,
                             Timestamp createdDate,
                             Timestamp lastModifiedDate,
                             BeerOrder beerOrder,
                             UUID beerId,
+                            String upc,
                             Integer orderQuantity,
                             Integer quantityAllocated) {
         super(id, version, createdDate, lastModifiedDate);
         this.beerOrder = beerOrder;
         this.beerId = beerId;
+        this.upc = upc;
         this.orderQuantity = orderQuantity;
         this.quantityAllocated = quantityAllocated;
     }
