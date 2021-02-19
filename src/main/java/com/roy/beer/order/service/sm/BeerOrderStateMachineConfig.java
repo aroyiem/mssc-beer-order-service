4cc483b81a73e4a05ff13fe5f5e12138dfbf9443
Anirban Roy <aroyiem@gmail.com>
2021-02-19 12:19:24 +0000

    setup allocation state machine

---
+++ src/main/java/com/roy/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -0,0 +1,29 @@
+package com.roy.beer.order.service.sm;
+
+import com.roy.beer.order.service.domain.BeerOrderEventEnum;
+import com.roy.beer.order.service.domain.BeerOrderStatusEnum;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+
+import java.util.EnumSet;
+
+@Configuration
+@Slf4j
+@EnableStateMachineFactory
+public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<BeerOrderStatusEnum, BeerOrderEventEnum> {
+
+    @Override
+    public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> states) throws Exception {
+        states.withStates()
+                .initial(BeerOrderStatusEnum.NEW)
+                .states(EnumSet.allOf(BeerOrderStatusEnum.class))
+                .end(BeerOrderStatusEnum.PICKED_UP)
+                .end(BeerOrderStatusEnum.DELIVERED)
+                .end(BeerOrderStatusEnum.DELIVERY_EXCEPTION)
+                .end(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
+                .end(BeerOrderStatusEnum.DELIVERED);
+    }
+}
